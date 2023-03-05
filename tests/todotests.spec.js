38752836a0a6353cc4b6a21e7fc80ee1f84a7a29
TheAbsintheDuck <gabriel-carlsson@hotmail.com>
2023-03-05 21:01:42 +0100

    Gabriel tester

--- tests/todotests.spec.js
+++ tests/todotests.spec.js
@@ -1,34 +1,33 @@
 const { test, expect } = require('@playwright/test');
 
-const newTodo = page.getByPlaceholder('What needs to be done?');
-
-const todos = [
-    'Watch a Brad Pitt Movie',
-    'Kidnap Brad Pitt',
-    'Make Brad Pitt love me'
-];
-
 test('add note', async ({ page }) => {
 
-    await page.goto('http://127.0.0.1:5500/index.html');
+  const newTodo = page.locator('[placeholder="What needs to be done?"]');
+  const todos = ['Watch a Brad Pitt Movie', 'Kidnap Brad Pitt', 'Make Brad Pitt love me'];
 
-    await newTodo.fill(todos[0]);
-    await newTodo.press('Enter');
+  await page.goto('http://127.0.0.1:5500/index.html');
 
-    await expect(page.locator('li p')).toHaveText(todos[0]);
+  await newTodo.fill(todos[0]);
+  await newTodo.press('Enter');
 
+  await expect(page.locator('li p')).toHaveText(todos[0]);
 });
 
-test('1 item', async ({ page }) => {
+test('1 item - 0 items', async ({ page }) => {
 
-    await page.goto('http://127.0.0.1:5500/index.html');
+  const newTodo = page.locator('[placeholder="What needs to be done?"]');
+  let checkbox = page.locator('#item-checkbox');
+  const todos = ['Watch a Brad Pitt Movie', 'Kidnap Brad Pitt', 'Make Brad Pitt love me'];
 
-    await newTodo.fill(todos[0]);
-    await newTodo.press('Enter');
+  await page.goto('http://127.0.0.1:5500/index.html');
 
-    await expect(page.locator('counter')).toHaveText("1 item left");
-    await page.locator('item-checkbox').toBeVisible();
-    await page.check('item-checkbox');
-    await page.isChecked('item-checkbox').toBeTruthy();
-    await expect(page.locator('counter')).toHaveText("0 items left");
+  await newTodo.fill(todos[0]);
+  await newTodo.press('Enter');
+
+  await expect(page.locator('#counter')).toHaveText('1 item left');
+
+  checkbox = await page.locator('#item-checkbox');
+  await checkbox.click();
+
+  await expect(page.locator('#counter')).toHaveText('0 items left');
 });
