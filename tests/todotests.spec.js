c4949476256cf8ce977f5963db0aa339f15a0bc1
1256ID <112385041+1256ID@users.noreply.github.com>
2023-03-07 17:50:26 +0100

    test

--- tests/todotests.spec.js
+++ tests/todotests.spec.js
@@ -31,3 +31,39 @@ test('1 item - 0 items', async ({ page }) => {
 
   await expect(page.locator('#counter')).toHaveText('0 items left');
 });
+
+test('remove notes', async ({ page }) => { 
+
+  const newTodo = page.locator('[placeholder="What needs to be done?"]');
+  let checkbox = page.locator('#item-checkbox');
+  const todos = ['A', 'B', 'C', 'D', 'E', 'F'];
+
+  await page.goto('http://127.0.0.1:5500/index.html');
+
+  await newTodo.fill(todos[0]);
+  await newTodo.press('Enter');
+  await newTodo.fill(todos[1]);
+  await newTodo.press('Enter');
+  await newTodo.fill(todos[2]);
+  await newTodo.press('Enter');
+  await newTodo.fill(todos[3]);
+  await newTodo.press('Enter');
+  await newTodo.fill(todos[4]);
+  await newTodo.press('Enter');
+  await newTodo.fill(todos[5]);
+  await newTodo.press('Enter');
+
+  await expect(page.locator('#counter')).toHaveText('6 item left');
+
+  checkbox = await page.locator('#item-checkbox');
+  await checkbox.click();
+
+  let deleteButton = document.getElementById('delete');
+
+  await deleteButton.click();
+
+  await expect(page.locator('#counter')).toHaveText('0 item left');
+
+
+});
+
